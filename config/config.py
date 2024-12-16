bbc8655a858990ed55b00794983e531766753bc9
Andrea Kangethe <152164886+AndreaKangethe@users.noreply.github.com>
2024-12-16 10:56:10 +0300

    Add files via upload

---
+++ config/config.py
@@ -0,0 +1,7 @@
+class Config:
+    input_size = 30  # Example feature size
+    hidden_size = 64
+    output_size = 2  # Binary classification
+    learning_rate = 0.001
+    batch_size = 64
+    num_epochs = 20
